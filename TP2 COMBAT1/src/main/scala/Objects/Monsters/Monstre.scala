861f8171d2d712faa1798c8ba4de8bf22a5ea7ce
Unknown <yves-patrick-albert.sanou1@uqac.ca>
2019-03-26 17:50:06 -0400

    base
    rdd de montres, envoi et reception de messages

--- TP2 COMBAT1/src/main/scala/Objects/Monsters/Monstre.scala
+++ TP2 COMBAT1/src/main/scala/Objects/Monsters/Monstre.scala
@@ -1,6 +1,15 @@
-class Monstre(nameVal:String) extends Serializable
+import java.awt.Point
+
+import scala.util.Random
+
+class Monstre(nameVal:String,pos:Array[Integer]) extends Serializable
 {
+
+
   val name=nameVal;
+  //var position=generateRandomPosition();
+  var position=pos;
+  var closestMonster:Monstre=null;
 
   def attaque(): Unit =
   {
@@ -12,7 +21,14 @@ class Monstre(nameVal:String) extends Serializable
     System.out.println(name+" a recu des degats");
   }
 
+   def generateRandomPosition():Array[Integer]={
 
+     var rd=new Random();
+     var res=new Array[Integer](2);
+     res(0)=rd.nextInt(100);
+     res(1)=rd.nextInt(100);
+     res;
+   }
 
   /*override def toString():String=
   {
--- TP2 COMBAT1/src/main/scala/Program.scala
+++ TP2 COMBAT1/src/main/scala/Program.scala
@@ -1,30 +1,81 @@
+import java.awt.Point
 import java.util
 import java.util.ArrayList
 
 import org.apache.spark.{SparkConf, SparkContext}
 
-class Program {
+import scala.collection.mutable.ListBuffer
+
+class Program extends Serializable {
 
   def run(): Unit = {
     val conf = new SparkConf().setMaster("local").setAppName("combat");
     val sc = new SparkContext(conf);
-    val array=init();
-    val MonstersRDD=sc.parallelize(array);
-    MonstersRDD.map(m=>sendMessages(m));
-    val a=MonstersRDD.collect();
+    val monsters=init();
+    val monstersRDD=sc.parallelize(monsters);
+    val positions=monstersRDD.flatMap(m=>sendPositions(m,monsters)).groupByKey().map(findClosest).collect();
+    val monstersRDD2=monstersRDD.map(m=>initializeGraph(m,positions));
+    val a=monstersRDD2.collect();
     var b=0;
   }
 
-  def sendMessages(monstre: Monstre): Monstre =
-  {
+
+  def initializeGraph(monstre:Monstre,positions:Array[(Monstre,Monstre)]):Monstre={
+    for(i<- 0 until positions.size)
+      {
+        if(monstre.name.equals(positions(i)._1.name))
+          {
+            monstre.closestMonster=positions(i)._2;
+          }
+      }
     monstre;
   }
 
-  def init(): Array[Monstre] = {
-    val m1 = new Monstre("m1");
-    val m2 = new Monstre("m2");
-    val m3 = new Monstre("m3");
+  def sendPositions(monstre: Monstre,monstres:List[Monstre]): List[(Monstre,Monstre)] =
+  {
+    var result=new ListBuffer[(Monstre,Monstre)];
+    for(m<-monstres)
+    {
+      if(!m.name.equals(monstre.name)) {
+        result.append((m, monstre));
+      }
+    }
+    result.toList;
+  }
+
+  def findClosest(item:(Monstre,Iterable[Monstre])): (Monstre,Monstre) =
+  {
+    val monsters=item._2.toList;
+    var closest=new Monstre("",null);
+    var distance=1000000000.0;
+    for(i<- 0 until monsters.size)
+    {
+      var temp=monsters(i);
+      if(!item._1.name.equals(temp.name)) {
+        var pA = new Point(item._1.position(0), item._1.position(1));
+        var pB = new Point(temp.position(0), temp.position(1));
+        if (distance > pA.distance(pB)) {
+          closest = temp;
+          distance=pA.distance(pB);
+        }
+      }
+    }
+    (item._1,closest);
+  }
+
+  def init(): List[Monstre] = {
+    var pos=new Array[Integer](2);
+    pos(0)=10;
+    pos(1)=5;
+    val m1 = new Monstre("m1",pos.clone());
+    pos(0)=0;
+    pos(1)=5;
+    val m2 = new Monstre("m2",pos.clone());
+    pos(0)=0;
+    pos(1)=10;
+    val m3 = new Monstre("m3",pos.clone());
+
     val result=Array(m1,m2,m3);
-    result;
+    result.toList;
   }
 }
