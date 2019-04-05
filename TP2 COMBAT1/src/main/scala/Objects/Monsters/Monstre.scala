252ea951e05a86d747dd54ddf664aee9b97ba447
Unknown <yves-patrick-albert.sanou1@uqac.ca>
2019-04-05 14:16:06 -0400

    envoi recption de messages

--- TP2 COMBAT1/src/main/scala/Objects/Monsters/Monstre.scala
+++ TP2 COMBAT1/src/main/scala/Objects/Monsters/Monstre.scala
@@ -2,36 +2,111 @@ import java.awt.Point
 
 import scala.util.Random
 
-class Monstre(nameVal:String,pos:Array[Integer]) extends Serializable
-{
+class Monstre(nameVal: String, pos: Array[Integer], fc: Integer) extends Serializable {
 
 
-  val name=nameVal;
+  val name = nameVal;
+  val rayonAction = 5;
+  val faction = fc
+  var alive = true;
+  val seuil = 5;
+  var hit = 0;
+  var healingPotions = 2;
   //var position=generateRandomPosition();
-  var position=pos;
-  var closestMonster:Monstre=null;
+  var position = pos;
+  var closestFoe: Monstre = null;
+  var closestAlly: Monstre = null;
 
-  def attaque(): Unit =
-  {
-    System.out.println(name+" attaque");
+  def attaque(): Unit = {
+    System.out.println(name + " attaque");
   }
 
-  def prendreDegats(): Unit =
-  {
-    System.out.println(name+" a recu des degats");
+  def prendreDegats(): Unit = {
+    System.out.println(name + " a recu des degats");
   }
 
-   def generateRandomPosition():Array[Integer]={
+  def generateRandomPosition(): Array[Integer] = {
 
-     var rd=new Random();
-     var res=new Array[Integer](2);
-     res(0)=rd.nextInt(100);
-     res(1)=rd.nextInt(100);
-     res;
-   }
+    var rd = new Random();
+    var res = new Array[Integer](2);
+    res(0) = rd.nextInt(100);
+    res(1) = rd.nextInt(100);
+    res;
+  }
 
   /*override def toString():String=
   {
     "{name:"+name+",id:1}";
   }*/
+
+  def intelligence(): (Monstre, String) = {
+    var res: (Monstre, String) = null;
+      if (closestAlly != null && closestAlly.hit > 0 && healingPotions > 0) {
+          var a = new Point(position(0), position(1));
+          var b = new Point(closestFoe.position(0), closestFoe.position(1));
+          var distance = a.distance(b);
+          if (distance < rayonAction) {;
+             return (this, heal());
+          }
+          else {
+            return (this, moveToHeal());
+          }
+      }
+      else if (closestFoe != null) {
+        var a = new Point(position(0), position(1));
+        var b = new Point(closestFoe.position(0), closestFoe.position(1));
+        var distance = a.distance(b);
+        if (distance < rayonAction) {
+          return (this, attack());
+        }
+        else {
+          return (this, moveToAttack());
+        }
+      }
+    else
+        return (this,noAction())
+  }
+
+  def attack(): String = {
+    return ("attack");
+  }
+
+  def moveToAttack(): String = {
+    return "moveToAttack";
+  }
+
+  def moveToHeal(): String = {
+    return "moveToHeal";
+  }
+
+  def heal(): String = {
+    return "heal";
+  }
+
+  def noAction(): String = {
+    return "noAction";
+  }
+
+  def takeHit(): Unit = {
+    hit += 1;
+    if (hit == seuil) {
+      alive = false;
+    }
+  }
+
+  def moveTowards(newPos: Array[Integer]): Unit = {
+    position = newPos;
+   /* var dx=(Double)(newPos(0)-position(0));
+    var dy=(Double)(newPos(1)-position(1));
+    var angle=Math.atan2(dx,dy);*/
+
+  }
+
+  def beHealed(): Unit = {
+    hit -= 1;
+  }
+
+  def consumePotion(): Unit = {
+    healingPotions -= 1;
+  }
 }
--- TP2 COMBAT1/src/main/scala/Program.scala
+++ TP2 COMBAT1/src/main/scala/Program.scala
@@ -11,71 +11,181 @@ class Program extends Serializable {
   def run(): Unit = {
     val conf = new SparkConf().setMaster("local").setAppName("combat");
     val sc = new SparkContext(conf);
-    val monsters=init();
-    val monstersRDD=sc.parallelize(monsters);
-    val positions=monstersRDD.flatMap(m=>sendPositions(m,monsters)).groupByKey().map(findClosest).collect();
-    val monstersRDD2=monstersRDD.map(m=>initializeGraph(m,positions));
-    val a=monstersRDD2.collect();
-    var b=0;
+    sc.setLogLevel("WARN");
+    var monsters = init();
+    var boucle = true;
+    var i=1;
+    while (boucle) {
+      Console.println("Tour "+i)
+      val monstersRDD = sc.parallelize(monsters);
+      val positions = monstersRDD.flatMap(m => sendPositions(m, monsters)).groupByKey().map(findClosest).collect();
+      val monstersRDD2 = monstersRDD.map(m => initializeGraph(m, positions));
+      var a=monstersRDD2.collect();
+      val messages = monstersRDD2.flatMap(m => sendMessages(m)).groupByKey();
+      if (messages.count() == 0)
+        boucle = false;
+      else {
+        val newMonsters = messages.map(mess => processMessages(mess)).collect();
+        val monstersRDD3 = monstersRDD2.flatMap((m => updateGraph(m, newMonsters.toList)));
+        monsters = monstersRDD3.collect().toList;
+        i+=1;
+        var a = 0;
+      }
+
+    }
+
+    Console.println("Combat terminé");
+    Console.println("Vainqueur: "+monsters(0).name);
+
+    var b = 0;
   }
 
 
-  def initializeGraph(monstre:Monstre,positions:Array[(Monstre,Monstre)]):Monstre={
-    for(i<- 0 until positions.size)
+  def initializeGraph(monstre: Monstre, positions: Array[(Monstre, (Monstre,Monstre))]): Monstre = {
+    for (i <- 0 until positions.size) {
+      if (monstre.name.equals(positions(i)._1.name)) {
+        monstre.closestFoe = positions(i)._2._1;
+        monstre.closestAlly=positions(i)._2._2;
+      }
+    }
+    if(positions.size==0)
       {
-        if(monstre.name.equals(positions(i)._1.name))
-          {
-            monstre.closestMonster=positions(i)._2;
-          }
+        monstre.closestFoe=null;
+        monstre.closestAlly=null;
       }
     monstre;
   }
 
-  def sendPositions(monstre: Monstre,monstres:List[Monstre]): List[(Monstre,Monstre)] =
-  {
-    var result=new ListBuffer[(Monstre,Monstre)];
-    for(m<-monstres)
-    {
-      if(!m.name.equals(monstre.name)) {
+  def sendPositions(monstre: Monstre, monstres: List[Monstre]): List[(Monstre, Monstre)] = {
+    var result = new ListBuffer[(Monstre, Monstre)];
+    for (m <- monstres) {
+      if (!m.name.equals(monstre.name)) {
         result.append((m, monstre));
       }
     }
-    result.toList;
+    var a=result.toList;
+    a;
   }
 
-  def findClosest(item:(Monstre,Iterable[Monstre])): (Monstre,Monstre) =
-  {
-    val monsters=item._2.toList;
-    var closest=new Monstre("",null);
-    var distance=1000000000.0;
-    for(i<- 0 until monsters.size)
-    {
-      var temp=monsters(i);
-      if(!item._1.name.equals(temp.name)) {
+  def findClosest(item: (Monstre, Iterable[Monstre])): (Monstre, (Monstre,Monstre)) = {
+    val monsters = item._2.toList;
+    var closestFoe:Monstre=null;
+    var closestAlly:Monstre=null;
+    var distanceFoe = 1000000000.0;
+    var distanceAlly = 1000000000.0;
+    for (i <- 0 until monsters.size) {
+      var temp = monsters(i);
+      if (!item._1.name.equals(temp.name)) {
         var pA = new Point(item._1.position(0), item._1.position(1));
         var pB = new Point(temp.position(0), temp.position(1));
-        if (distance > pA.distance(pB)) {
-          closest = temp;
-          distance=pA.distance(pB);
+        var distance=pA.distance(pB);
+        if(item._1.faction==temp.faction)
+          {
+            if (distanceAlly > distance) {
+              closestAlly = temp;
+              distanceAlly = pA.distance(pB);
+            }
+          }
+        else
+          {
+            if (distanceFoe > distance) {
+              closestFoe = temp;
+              distanceFoe = pA.distance(pB);
+            }
+          }
+
+      }
+    }
+    (item._1,( closestFoe,closestAlly));
+  }
+
+  def sendMessages(monstre: Monstre): List[(Monstre, (String, String))] = {
+    var res=new ListBuffer[(Monstre, (String, String))];
+    var decision = monstre.intelligence();
+    var action = decision._2;
+    var emetteur = decision._1;
+    action match {
+      case "attack" => res.append((emetteur.closestFoe, ("attack", emetteur.name)))
+      case "moveToAttack" => res.append((emetteur, ("move", emetteur.closestFoe.name)))
+      case "moveToHeal" => res.append((emetteur, ("move", emetteur.closestAlly.name)))
+      case "heal" => {res.append((emetteur.closestAlly, ("heal", emetteur.name)));
+                      res.append((emetteur, ("potionUsed", "")))}
+      case "noAction"=>{}
+    }
+    res.toList;
+  }
+
+  def processMessages(item: (Monstre, Iterable[(String, String)])): Monstre = {
+    var monstre = item._1;
+    var messages = item._2.toList;
+    for (m <- messages) {
+      var action = m._1;
+      action match {
+        case "attack" => {
+          monstre.takeHit();
+          Console.println(m._2 + " attaque " + monstre.name);
+          if (!monstre.alive)
+            {
+            Console.println(monstre.name + " a ete tué");
+            }
+        }
+        case "move" => {
+          var target:Monstre=null;
+          var text:String=null;
+          if(monstre.closestFoe.name.equals(m._2)) {
+            target = monstre.closestFoe;
+            text=" se deplace vers l'enemi ";
+          }
+          else {
+            target = monstre.closestAlly;
+            text=" se deplace vers l'allié "
+          }
+          monstre.moveTo(target.position);
+          Console.println(monstre.name + text + m._2);
+        }
+        case "heal" => {
+          monstre.beHealed();
+          Console.println(monstre.name + " est soigné par " + m._2);
+        }
+        case "potionUsed" => {
+          monstre.consumePotion();
         }
       }
     }
-    (item._1,closest);
+    monstre;
+  }
+
+  def updateGraph(monstre: Monstre, newMonstres: List[Monstre]): List[Monstre] = {
+
+    var res = new ListBuffer[Monstre];
+      var item = monstre;
+      for (m <- newMonstres) {
+        if (m.name.equals(item.name))
+          item = m;
+      }
+    if(item.alive)
+      res.append(item);
+    res.toList;
   }
 
   def init(): List[Monstre] = {
-    var pos=new Array[Integer](2);
-    pos(0)=10;
-    pos(1)=5;
-    val m1 = new Monstre("m1",pos.clone());
-    pos(0)=0;
-    pos(1)=5;
-    val m2 = new Monstre("m2",pos.clone());
-    pos(0)=0;
-    pos(1)=10;
-    val m3 = new Monstre("m3",pos.clone());
-
-    val result=Array(m1,m2,m3);
+    var pos = new Array[Integer](2);
+    pos(0) = 10;
+    pos(1) = 5;
+    val m1 = new Monstre("m1", pos.clone(),1);
+    pos(0) = 0;
+    pos(1) = 5;
+    val m2 = new Monstre("m2", pos.clone(),1);
+    pos(0) = 0;
+    pos(1) = 10;
+    val m3 = new Monstre("m3", pos.clone(),2);
+    pos(0) = 20;
+    pos(1) = 10;
+    val m4 = new Monstre("m4", pos.clone(),2);
+    pos(0) = 20;
+    pos(1) = 15;
+    val m5 = new Monstre("m5", pos.clone(),2);
+    val result = Array(m1, m2, m3,m4,m5);
     result.toList;
   }
 }
